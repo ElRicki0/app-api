6b67a3e175a5979ebfad70bc219831c343756e80
Ricardo Nicolas Melara <107507393+ElRicki0@users.noreply.github.com>
2024-05-29 09:15:41 -0600

    Update BottonTab.js

--- src/navegation/BottonTab.js
+++ src/navegation/BottonTab.js
@@ -6,6 +6,7 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import PokemonList from '../screens/PokemonList';
 import HomeScreen from '../screens/HomeScreen';
 import PokemonAxios from '../screens/PokemonAxios';
+import APIperros from '../screens/APIperros';
 
 // Navegador Bottom Tabs Navigator
 const Tab = createBottomTabNavigator();
@@ -13,54 +14,71 @@ const Tab = createBottomTabNavigator();
 export default function BottomTab() {
   return (
     <Tab.Navigator>
-    <Tab.Screen
-      name="HomeScreen"
-      component={HomeScreen}
-      options={{
-        title: 'Inicio',
-        tabBarActiveTintColor: '#FFC300', // Color activo de la pestaña
-        headerStyle: {
-          backgroundColor: '#FFC300', // Color del header
-        },
-        headerTintColor: '#fff', // Color del texto en el header
-        tabBarIcon: ({ color }) => ( // Función que define el ícono de la pestaña
-          <Ionicons name="home" color={color} size={24} /> // `color` proviene de React Navigation
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="PokemonAxios"
-      component={PokemonAxios}
-      options={{
-        title: 'Lista Pokemon con Axios',
-        tabBarActiveTintColor: '#cc0000',
-        headerStyle: {
-          backgroundColor: '#cc0000',
-          borderBottomRightRadius: 35,
-          borderBottomLeftRadius: 35,
-        },
-        headerTintColor: '#fff',
-        tabBarIcon: ({ color }) => (
-          <Ionicons name="list" color={color} size={24} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="PokemonList"
-      component={PokemonList}
-      options={{
-        title: 'Lista Pokemon Fetch',
-        tabBarActiveTintColor: '#3b4cca',
-        headerStyle: {
-          backgroundColor: '#3b4cca',
-        },
-        headerTintColor: '#fff',
-        tabBarIcon: ({ color }) => (
-          <Ionicons name="list" color={color} size={24} />
-        ),
-      }}
-    />
-  </Tab.Navigator>
-  
+      <Tab.Screen
+        name="HomeScreen"
+        component={HomeScreen}
+        options={{
+          title: 'Inicio',
+          tabBarActiveTintColor: '#FFC300', // Color activo de la pestaña
+          headerStyle: {
+            backgroundColor: '#FFC300', // Color del header
+          },
+          headerTintColor: '#fff', // Color del texto en el header
+          tabBarIcon: ({ color }) => ( // Función que define el ícono de la pestaña
+            <Ionicons name="home" color={color} size={24} /> // `color` proviene de React Navigation
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="PokemonAxios"
+        component={PokemonAxios}
+        options={{
+          title: 'Lista Pokemon con Axios',
+          tabBarActiveTintColor: '#cc0000',
+          headerStyle: {
+            backgroundColor: '#cc0000',
+            borderBottomRightRadius: 35,
+            borderBottomLeftRadius: 35,
+          },
+          headerTintColor: '#fff',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="list" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="PokemonList"
+        component={PokemonList}
+        options={{
+          title: 'Lista Pokemon Fetch',
+          tabBarActiveTintColor: '#3b4cca',
+          headerStyle: {
+            backgroundColor: '#3b4cca',
+          },
+          headerTintColor: '#fff',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="list" color={color} size={24} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="APIperros"
+        component={APIperros}
+        options={{
+          title: 'Api perro',
+          tabBarActiveTintColor: '#3b4cca',
+          headerStyle: {
+            backgroundColor: '#3b4cca',
+          },
+          headerTintColor: '#fff',
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="list" color={color} size={24} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+
+
+
   );
 }
